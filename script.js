492ec7183a0fe26457a15baebf7d911fad6fbd46
Vinicius Pessoa <vini.pessoa99@gmail.com>
2021-08-08 19:20:40 -0300

    Organização do js

--- script.js
+++ script.js
@@ -1,4 +1,46 @@
-let abas = []
+function submeter() {
+
+  const editables = ["name", "subtitle", "slogan"];
+
+  editables.forEach(editable => {
+    var valor = document.getElementById(editable).textContent
+    document.getElementById(editable).contentEditable = false;
+    console.log(`${editable}: ${valor}`);
+  });
+
+}
+
+function insertSM (params) {
+  
+  var nomeClasse = params.children[0].className;
+  var redeSocial = nomeClasse.baseVal.replace("feather feather-","");
+  document.getElementById("form-user-sm").style.display = "inline-block";
+  
+  switch (redeSocial) {
+    case "instagram":
+    console.log("ENTROU!");
+    document.getElementById("label-url-sm").textContent = "https://www.instagram.com/";
+    document.getElementById("input-sm").setAttribute("placeholder","User Instagram");
+      break;
+  
+  }
+
+}
+
+function submeteSM() {
+  document.getElementById("form-user-sm").style.display = "none";
+  var urlSM = document.getElementById("label-url-sm").textContent;
+  var user = document.getElementById("input-sm").value;
+  var url = urlSM + user;
+  console.log(user);
+
+  if (urlSM.includes("instagram")){
+    document.getElementById("a-instagram").setAttribute("href", url);
+  }
+
+}
+
+/* let abas = []
 
 abas.forEach(aba => {
 
@@ -19,3 +61,4 @@ abas.forEach(aba => {
   }
 
 })
+ */
