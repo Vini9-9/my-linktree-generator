ae3a4fdec298f148dcc69ae5e3fd02abd9c93c35
Vinicius Pessoa <vini.pessoa99@gmail.com>
2021-08-09 00:10:06 -0300

    Ajustes na inclusão de redes sociais

--- script.js
+++ script.js
@@ -5,29 +5,45 @@ function submeter() {
   editables.forEach(editable => {
     var valor = document.getElementById(editable).textContent
     document.getElementById(editable).contentEditable = false;
-    console.log(`${editable}: ${valor}`);
   });
 
 }
 
 function insertSM (params) {
   
-/*   console.log(params.previousElementSibling.children[0].className); */
+/*   params.previousElementSibling.style = "background-color: #330249;"; */
+  params.previousElementSibling.className = "SM-active";
   var nomeClasse = params.previousElementSibling.children[0].className;
   var redeSocial = nomeClasse.baseVal.replace("feather feather-","");
 
   if (params.className == "im im-angle-down") {
     params.className = "im im-angle-up";
     document.getElementById("form-user-sm").style.display = "inline-block";
-    
+    var url;
+    var placeHolder = "User " + redeSocial;
+
     switch (redeSocial) {
       case "instagram":
-      document.getElementById("label-url-sm").textContent = "https://www.instagram.com/";
-      document.getElementById("input-sm").setAttribute("placeholder","User Instagram");
+        url = "https://www.instagram.com/";
         break;
-    
+      case "github":
+        url = "https://github.com/"
+        break;
+      case "linkedin":
+        url = "https://www.linkedin.com/in/"
+        break;
+      case "youtube":
+        url = "https://www.youtube.com/channel/"
+        placeHolder = placeHolder.replace("User","Canal");
+        break;
     }
+
+
+    document.getElementById("label-url-sm").textContent = "URL: " + url.replace("https://","");
+    document.getElementById("input-sm").setAttribute("placeholder",placeHolder);
+
   } else {
+    params.previousElementSibling.classList.remove("SM-active");
     params.className = "im im-angle-down";
     document.getElementById("form-user-sm").style.display = "none";
   }
@@ -38,16 +54,13 @@ function insertSM (params) {
 
 function submeteSM() {
   document.getElementById("form-user-sm").style.display = "none";
-  var urlSM = document.getElementById("label-url-sm").textContent;
+  var urlSM = document.getElementById("label-url-sm").textContent.replace("URL: ","");
   var user = document.getElementById("input-sm").value.trim();
-  var url = urlSM + user;
-  console.log(user);
+  var url = "https://" + urlSM + user;
 
-  if (urlSM.includes("instagram")){
-    document.getElementById("a-instagram").setAttribute("href", url);
-  }
-
+  document.getElementsByClassName("SM-active")[0].setAttribute("href", url);
   document.getElementsByClassName("im im-angle-up")[0].className = "im im-angle-down";
+  document.getElementsByClassName("SM-active")[0].classList.remove("SM-active");
 }
 
 /* let abas = []
